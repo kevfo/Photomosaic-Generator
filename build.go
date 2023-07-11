756e9400351cebb892a7dcf0f209bb48adcdd89f
kevfo <75442525+kevfo@users.noreply.github.com>
2023-07-11 16:18:09 +0800

    -Added image processing functionality

--- build.go
+++ build.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image/png"
+	"os"
 
 	// Custom modules for this project
+	"github.com/kevfo/photomosaic_generator/photo"
 	_ "github.com/kevfo/photomosaic_generator/photo"
 	"github.com/kevfo/photomosaic_generator/start"
 )
@@ -13,11 +16,22 @@ func main() {
 	inputImage := flag.String("img", "image.png", "The name of the image that you want to convert into a photomosaic.")
 	outputName := flag.String("out", "result.png", "The name of the resulting photomosaic.")
 	library := flag.String("lib", "pictures", "The name of a folder of photos that you want to use to construct the photomosaic.")
-	numTiles := flag.Int("tiles", 10, "The size of tiles per row that you want the photomosaic to have.")
+	numTiles := flag.Int("tiles", 20, "The size of tiles per row that you want the photomosaic to have.")
 	flag.Parse()
 
 	database := start.InitDatabase(*library)
-	fmt.Println(database)
+	img, err := os.Open(*inputImage)
+	if err != nil {
+		fmt.Printf("An error happened while reading in %s: %s\n", *inputImage, err)
+		os.Exit(1)
+	}
+	defer img.Close()
+
+	toUse, err := png.Decode(img)
+	if err != nil {
+		fmt.Printf("Unable to decode %s - are you sure %s is a PNG image?", img.Name(), img.Name())
+	}
+	photo.GenerateMosaic(*outputName, toUse, *numTiles, database)
 
 	fmt.Println(*inputImage, *outputName, *numTiles)
 }
