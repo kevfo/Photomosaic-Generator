0d745647661d44612cfc5b100d7a2d45e73692f4
kevfo <75442525+kevfo@users.noreply.github.com>
2023-07-11 16:01:25 +0800

    Finished first iter of generating photomosaic

--- photo/photo.go
+++ photo/photo.go
@@ -1,29 +1,49 @@
 package photo
 
 import (
+	"fmt"
 	"image"
+	"image/png"
 	"math"
+	"os"
 
 	// start module import
-	_ "../start"
+	"../start"
 )
 
-/*
-1) Divide image up into however many tiles
-2) Get RGBA value of tile
-3) Replace tile with closest value
-4) Repeat 1) to 3) until all tiles done.
-*/
-func GenerateMosaic(output string, original image.Image, tileSize int) image.Image {
+func GenerateMosaic(output string, original image.Image, tileSize int, data map[string][3]float64) {
 	oriBounds := original.Bounds()
 	mosaic, height, width := image.NewRGBA(image.Rect(0, 0, oriBounds.Dx(), oriBounds.Dy())), oriBounds.Dy(), oriBounds.Dx()
 
-	for y := height - 1; y >= 0; y -= tileSize {
+	for y := height - tileSize; y >= 0; y -= tileSize {
 		for x := 0; x < width; x += tileSize {
+			crop := image.Rect(x, y, x+tileSize, y+tileSize)
+			subImg := original.(interface {
+				SubImage(r image.Rectangle) image.Image
+			}).SubImage(crop).(image.Image)
 
+			tileName := findClosest(start.ProcessImage(subImg), data)
+			openTile, err := os.Open(tileName)
+			if err != nil {
+				fmt.Printf("Could not open the following image: '%s'\n", tileName)
+				os.Exit(1)
+			}
+			oriTile, err := png.Decode(openTile)
+			if err != nil {
+				fmt.Printf("Could not decode the image '%s'\n", tileName)
+				os.Exit(1)
+			}
+			tile := resizeImage(oriTile, tileSize, tileSize)
+
+			// Replacing the image here:
+			for x := crop.Min.X; x < crop.Max.X; x++ {
+				for y := crop.Min.Y; y < crop.Min.Y; y++ {
+					mosaic.Set(x, y, tile.At(x-crop.Min.X, y-crop.Min.Y))
+				}
+			}
 		}
 	}
-	return mosaic
+	outputImage(mosaic, output)
 }
 
 func distance(a1, a2 [3]float64) float64 {
@@ -55,4 +75,16 @@ func resizeImage(img image.Image, newWidth, newHeight int) image.Image {
 		}
 	}
 	return resizedImg
+}
+
+func outputImage(img image.Image, fileName string) {
+	filePath, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("Error creating '%s': %s\n", fileName, err)
+		os.Exit(1)
+	}
+	err = png.Encode(filePath, img)
+	if err != nil {
+		fmt.Printf("An error occurred while encoding: ")
+	}
 }
